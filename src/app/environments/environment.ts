53cc1bab89388623285f1ccd444027646689392d
mael6977 <mael4095437@gmail.com>
2024-06-21 17:59:33 -0500

    Update environment.ts

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,6 +1,6 @@
 import { version } from './version';
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3200',
+  apiUrl: 'https://servicesferretest.onrender.com/',
   version: version
 };
